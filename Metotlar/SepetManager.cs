6b51776895f0f18e16e2e1d93ddc9059f977d14c
görkem kışlıoğlu <gkslogl@gmail.com>
2021-06-25 15:27:03 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        // Manager,Service,Dal gibi ifadeler varsa bunlar operasyon tutuyor demektir.
+
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi:"+ urun.Adi);
+
+        }
+    }
+}
